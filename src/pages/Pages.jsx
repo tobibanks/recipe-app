a8304436583c48110d1ed41e415c00bbc06e5ba8
Tobi Odogwu (Tobi Banks) <63855026+tobibanks@users.noreply.github.com>
2023-09-04 23:37:38 +0100

    first push

---
+++ src/pages/Pages.jsx
@@ -0,0 +1,17 @@
+import Home from "./Home"
+import React from "react";
+import {Route, Routes, } from "react-router-dom";
+import Cuisine from "./Cuisine";
+
+function Pages() {
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/cuisine/:type" element={<Cuisine/>}/>
+    </Routes>
+   
+  )
+}
+
+export default Pages
